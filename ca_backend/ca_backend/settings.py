2716f1fbeaca8613a2406161ed9b535913d340fd
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2023-10-19 22:59:36 +0530

    [UPDATED] Allowed Hosts

--- ca_backend/ca_backend/settings.py
+++ ca_backend/ca_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6(bl3*7w2kdv^!)wvbt444&29i3b1^*xujif*7v3l30(r@94_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
