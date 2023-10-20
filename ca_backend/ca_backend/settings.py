6a3d2bea8853fb475e59ce03c76e94a0f11806f3
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2023-10-20 14:03:13 +0530

    [updated] static root

--- ca_backend/ca_backend/settings.py
+++ ca_backend/ca_backend/settings.py
@@ -154,6 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
