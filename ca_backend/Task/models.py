5df2abcf5efe2af83c169391a8db4ce85e5cc394
Kaustubh Mani Tripathi <129510465+kmtGryffindor20@users.noreply.github.com>
2023-12-19 15:09:52 +0530

    [ADDED] Task Submission, Task Verification by Admin, and Task Deadline (#58)
    * [ADDED] Avatar Id [UPDATED] Serializer Errors and Swagger Comments
    
    * [UPDATED] Referral code in Get Request
    
    * [ADDED] Task Submition, Task Verification by Admin and Task Deadline

--- ca_backend/Task/models.py
+++ ca_backend/Task/models.py
@@ -12,3 +12,16 @@ class Task(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(blank=True, null=False)
     points = models.IntegerField(null=False, default=0)
+    deadline = models.DateTimeField(null=False, blank=False, default="2024-01-01 00:00:00", editable=True)
+
+
+class TaskSubmission(models.Model):
+    """
+    Model for Task Submission.
+    Stores the Task, the User who submitted it and the Timestamp of the Submission.
+    """
+
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    user = models.ForeignKey("Authentication.UserProfile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    verified = models.BooleanField(default=False)
--- ca_backend/Task/views.py
+++ ca_backend/Task/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from rest_framework import generics, mixins, status
-from .models import Task
+from rest_framework import generics, mixins, status, views, permissions
+from .models import Task ,TaskSubmission
 from .serializers import TaskSerializer
 from Authentication.models import UserProfile
-from .serializers import LeaderboardSerializer
+from .serializers import LeaderboardSerializer , TaskSubmissionSerializer
 from rest_framework.response import Response
 from ca_backend.permissions import IsAdminUser, IsStaffUser
+from django.utils import timezone
 
 
 # Create your views here.
@@ -56,4 +57,69 @@ class TaskLeaderboardView(generics.GenericAPIView):
         all_users_pf = all_users_pf.order_by("-points")
         serializer = LeaderboardSerializer(all_users_pf, many=True)
         serializer_data = serializer.data
+        return Response(serializer_data, status=status.HTTP_200_OK)
+
+
+class SubmitTaskAPIView(views.APIView):
+    """
+    View for Submitting the Task. Can only be submitted by Verified Users.
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, task_id, *args, **kwargs):
+        user_id = request.user
+        task = Task.objects.filter(id=task_id).first()
+        if task.deadline < timezone.now():
+            return Response({"status": "Task Deadline Expired"}, status=status.HTTP_400_BAD_REQUEST)
+        user = UserProfile.objects.filter(user=user_id).first()
+        if TaskSubmission.objects.filter(task=task, user=user).exists():
+            return Response({"status: Task Already Submitted"},status=status.HTTP_400_BAD_REQUEST)
+        
+        task_submission = TaskSubmission(task=task, user=user)
+        
+        task_submission.save()
+        return Response(status=status.HTTP_200_OK)
+    
+
+class AdminVerifyTaskSubmissionAPIView(views.APIView):
+    """
+    View for Admin to verify the Task Submission. Can only be accessed by Admin/Staff User.
+    """
+    permission_classes = [IsStaffUser]
+
+    def post(self, request, task_submission_id, *args, **kwargs):
+        
+        if not TaskSubmission.objects.filter(id=task_submission_id).exists():
+            return Response({"status": "Task Submission Does Not Exist"}, status=status.HTTP_404_NOT_FOUND)
+        if TaskSubmission.objects.get(id=task_submission_id).verified:
+            return Response({"status": "Task Submission Already Verified"}, status=status.HTTP_400_BAD_REQUEST)
+        task_submission = TaskSubmission.objects.get(id=task_submission_id)
+        user = task_submission.user
+        task = task_submission.task
+        user.points += task.points
+        user.save()
+        task_submission.verified = True
+        task_submission.save()
+        return Response({"status": "Task Submission Verified"}, status=status.HTTP_200_OK)
+    
+
+class SubmittedUserTasksListAPIView(generics.GenericAPIView):
+    """
+    View for getting list of all unverified Tasks submitted by all users. Can only be accessed by Admin/Staff User.
+    """
+
+    permission_classes = [permissions.IsAuthenticated, IsAdminUser]
+    serializer_class = TaskSubmissionSerializer
+    queryset = TaskSubmission.objects.all()
+
+    def get(self, request):
+        if request.user.role < 2:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        task_submissions = TaskSubmission.objects.filter(verified=False)
+        serializer = TaskSubmissionSerializer(task_submissions, many=True)
+        try:
+            serializer_data = serializer.data
+        except Exception as e:
+            return Response({"status": "No Task Submission Found"}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer_data, status=status.HTTP_200_OK)
