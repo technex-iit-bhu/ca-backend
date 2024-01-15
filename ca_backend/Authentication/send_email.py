df11e059a39c5f21ebce2e1e0898a98f8ec1d4ae
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2024-01-15 22:50:32 +0530

    Revert "Merge branch 'dev'"
    This reverts commit 80f447d7cc0d68a9133d55005b59ccd202df1d1b, reversing
    changes made to c2e38bf6eb8e15c527525f448cd7fcf7173130ac.
    
    Resolved conflicts

--- ca_backend/Authentication/send_email.py
+++ ca_backend/Authentication/send_email.py
@@ -1,194 +1,32 @@
 from decouple import config
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-
-try:
-    SUPPORT_EMAIL = config("SUPPORT_EMAIL")
-except:
-    SUPPORT_EMAIL = "tech@technex.in"
-
-LOGO_FILE_PATH = "./ca_backend/logo/asset.png"
-
-with open(LOGO_FILE_PATH, 'rb') as fp:
-    IMAGE = MIMEImage(fp.read())
-
-    IMAGE.add_header('Content-ID', '<logo>')
-    IMAGE.add_header('Content-Disposition', 'inline')  # Set the Content-Disposition header to "inline"
-
 #todo: improve messages
-def send_email_cnf_email(rec_email, username, connection: smtplib.SMTP = None):
-    msg = MIMEMultipart()
-    msg['From'] = config("EMAIL_HOST_USER")
-    msg['To'] = rec_email
-    msg['Subject'] = "Technex'24 - Email Verification Successful"
-    # Use images and style the email
-    
-    if IMAGE:
-        msg.attach(IMAGE)
-    else:
-        with open(LOGO_FILE_PATH, 'rb') as fp:
-            img = MIMEImage(fp.read())
-        img.add_header('Content-ID', '<logo>')
-        img.add_header('Content-Disposition', 'inline')  # Set the Content-Disposition header to "inline"
-        msg.attach(img)
+def send_email_cnf_email(rec_email):
+    msg=f"Subject:Your Technex account\n\nYour email has been verified. We will contact you shortly with further procedures."
+    send(rec_email,msg)
 
-    html = f"""\
-    <html>
-        <head></head>
-        <body>
-        <center><img src="cid:logo" alt="logo" border="0" width="200" height="200"></center>
-            <h3>Dear {username},</h3>
-            <br>
-            <p>Great news! Your email address has been successfully verified for your Technex'24 account. Please wait for our admins to verify your account. Once that is done we will contact you and you will be able to log in to your account.</p>
-            <br>
-            If you have any additional questions or if there's anything else we can assist you with, feel free to reach out to our support team at {SUPPORT_EMAIL}.
+def send_email_verif_email(rec_email,token):
+    msg=f"Subject:Verify your Technex account\n\nPlease click on the following link to verify your email:\n\n{config('BACKEND_URL')}/auth/verifyemail/{token}"
+    send(rec_email,msg)
 
-            Thank you for being a part of Technex'24!
-            <br>
-            <p>Best regards,</p>
-            <p>CA Team - Technex'24</p>
+def send_approved_email(rec_email):
+    msg=f"Subject:Your Technex account\n\nYour account has been approved. You can now login to your account. \n\n CA Portal:  {config('FRONTEND_URL')}/"
+    send(rec_email,msg)
 
-        </body>
-    </html>
-    """
-    body = html
-    msg.attach(MIMEText(body, 'html'))
-    text = msg.as_string()
-    send(rec_email, text, connection)
+def send_otp_email(rec_email,otp):
+    msg=f"Subject:OTP for password reset\n\nYour OTP for resetting your password is {otp}.\n If you did not request this, ignore this mail and do not share your OTP."
+    send(rec_email,msg)
 
-
-def send_email_verif_email(rec_email, token, username, connection: smtplib.SMTP = None):
-    msg = MIMEMultipart()
-    msg['From'] = config("EMAIL_HOST_USER")
-    msg['To'] = rec_email
-    msg['Subject'] = "Technex'24 - Email Verification"
-    # Use images and style the email
-    
-
-    if IMAGE:
-        msg.attach(IMAGE)
-    else:
-        with open(LOGO_FILE_PATH, 'rb') as fp:
-            img = MIMEImage(fp.read())
-        img.add_header('Content-ID', '<logo>')
-        img.add_header('Content-Disposition', 'inline')  # Set the Content-Disposition header to "inline"
-        msg.attach(img)
-
-    html = f"""\
-    <html>
-        <head></head>
-        <body>
-        <center><img src="cid:logo" alt="logo" border="0" width="200" height="200"></center>
-            <h3>Dear {username},</h3>
-            <br>
-            <p>Congratulations! Your account on Technex'24 has been successfully created. Please click on the following button to confirm and activate your account</p>
-            <br>
-            <center><a href="{config('BACKEND_URL')}/auth/verifyemail/{token}/"><button style="background-color: #4CAF50; /* Green */
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    hover: cursor: pointer;">Verify Email</button></a></center>
-            <br>
-            <p>If you encounter any issues or have any questions, feel free to reach out to our support team at {SUPPORT_EMAIL}.</p>
-            <p>Best regards,</p>
-            <p>CA Team - Technex'24</p>
-
-            PS: If you are unable to click the button, copy and paste the following link in your browser: {config('BACKEND_URL')}/auth/verifyemail/{token}/
-        </body>
-    </html>
-    """
-    body = html
-    msg.attach(MIMEText(body, 'html'))
-    text = msg.as_string()
-    send(rec_email, text, connection)
-
-def send_approved_email(rec_email, username, connection: smtplib.SMTP = None):
-    msg = MIMEMultipart()
-    msg['From'] = config("EMAIL_HOST_USER")
-    msg['To'] = rec_email
-    msg['Subject'] = "Technex'24 - Account Approved"
-    # Use images and style the email
-    if IMAGE:
-        msg.attach(IMAGE)
-    else:
-        with open(LOGO_FILE_PATH, 'rb') as fp:
-            img = MIMEImage(fp.read())
-        img.add_header('Content-ID', '<logo>')
-        img.add_header('Content-Disposition', 'inline')  # Set the Content-Disposition header to "inline"
-        msg.attach(img)
-    html = f"""\
-    <html>
-        <head></head>
-        <body>
-        <center><img src="cid:logo" alt="logo" border="0" width="200" height="200"></center>
-            <h3>Dear {username},</h3>
-            <br>
-            <p>We are pleased to inform you that your Technex'24 account has been successfully verified. Thank you for completing the verification process promptly.</p>
-            <br>
-            <p>If you encounter any issues or have any questions, feel free to reach out to our support team at {SUPPORT_EMAIL}.</p>
-            <p>Best regards,</p>
-            <p>CA Team - Technex'24</p>
-        </body>
-    </html>
-    """
-    body = html
-    msg.attach(MIMEText(body, 'html'))
-    text = msg.as_string()
-    send(rec_email, text, connection)
-
-def send_otp_email(rec_email,otp, username, connection: smtplib.SMTP = None):
-    msg = MIMEMultipart()
-    msg['From'] = config("EMAIL_HOST_USER")
-    msg['To'] = rec_email
-    msg['Subject'] = "Technex'24 - Password Reset OTP"
-    # Use images and style the email
-    if IMAGE:
-        msg.attach(IMAGE)
-    else:
-        with open(LOGO_FILE_PATH, 'rb') as fp:
-            img = MIMEImage(fp.read())
-        img.add_header('Content-ID', '<logo>')
-        img.add_header('Content-Disposition', 'inline')  # Set the Content-Disposition header to "inline"
-        msg.attach(img)
-    html = f"""\
-    <html>
-        <head></head>
-        <body>
-        <center><img src="cid:logo" alt="logo" border="0" width="200" height="200"></center>
-            <h3>Dear {username},</h3>
-            <br>
-            <p>Here is your OTP for verification:</p>
-            <br>
-            <center><h1>{otp}</h1></center>
-            <br>
-            <p>Do not share this OTP with anyone. If you did not request this OTP, please ignore this email.</p>
-            <p>If you encounter any issues or have any questions, feel free to reach out to our support team at {SUPPORT_EMAIL}.</p>
-            <p>Best regards,</p>
-            <p>CA Team - Technex'24</p>
-        </body>
-    </html>
-    """
-    body = html
-    msg.attach(MIMEText(body, 'html'))
-    text = msg.as_string()
-    send(rec_email, text, connection)
-    
-
-def send(rec_email, msg, connection: smtplib.SMTP = None):
+def send(rec_email,msg):
     """
     Function to send verification email to the user 
     """
-    if connection:
-        connection.sendmail(config("EMAIL_HOST_USER"), rec_email, msg)
-    else:
-        connection = smtplib.SMTP("smtp.gmail.com", port=587)
-        connection.starttls()
-        connection.login(config("EMAIL_HOST_USER"), config("EMAIL_HOST_PASSWORD"))
-        connection.sendmail(config("EMAIL_HOST_USER"), rec_email, msg)
     
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=config("EMAIL_HOST_USER"), password=config("EMAIL_HOST_PASSWORD"))
+        connection.sendmail(from_addr=config("EMAIL_HOST_USER"), to_addrs=rec_email,msg=msg)
+        # connection.close()
+    
+
 
